8520e1a9abd2cfe6127ce22e5c2700f77c914738
108charlotte <108sarahsmith@gmail.com>
2025-06-21 14:00:26 -0400

    adjusted padding

--- personal-website/app/Skills.js
+++ personal-website/app/Skills.js
@@ -6,18 +6,18 @@ export default function Skills() {
     <FadeIn id="skills" className="justify-center overflow-y-auto h-screen w-full snap-start flex flex-col items-center justify-center">
       <h2 className="mb-4">Skills</h2>
       <div className="flex flex-col gap-y-4 items-start">
-        <div className="proficient flex flex-row gap-5 items-center justify-start bg-gray-800 rounded-lg p-3">
+        <div className="proficient flex flex-row gap-5 items-center justify-start bg-gray-800 rounded-lg px-6 py-4">
           <p className="w-24 font-semibold">Proficient</p>
           <Languages path="/python_logo.png" label="Python" />
           <Languages path="/html_logo.png" imgClassName="w-9 h-9" label="HTML" />
         </div>
-        <div className="familiar flex flex-row gap-5 items-center justify-start bg-gray-800 rounded-lg p-3 mx-auto">
+        <div className="familiar flex flex-row gap-5 items-center justify-start bg-gray-800 rounded-lg px-6 py-4 mx-auto">
           <p className="w-24 font-semibold">Familiar</p>
           <Languages path="/css_logo.png" imgClassName="w-9 h-9" label="CSS" />
           <Languages path="/javascript_logo.png" imgClassName="w-7 h-7" label="JavaScript" />
           <Frameworks path="/flask_logo.svg" label="Flask" />
         </div>
-        <div className="learning flex flex-row gap-6 items-center justify-start bg-gray-800 rounded-lg p-3">
+        <div className="learning flex flex-row gap-6 items-center justify-start bg-gray-800 rounded-lg px-6 py-4">
           <p className="w-24 font-semibold">Exploring</p>
           <Frameworks path="/react_logo.svg" label="React" />
           <Platforms path="/godot_logo.svg" label="Godot" />
@@ -45,7 +45,7 @@ function Platforms({ path, label }) {
       className="w-12 h-12 rounded border-4 border-purple-500 overflow-hidden flex items-center justify-center bg-white rotate-45"
       imgClassName="-rotate-45 w-8 h-8"
       label={label}
-      labelClassName="ml-4"
+      labelClassName="mt-3 sm:mt-0 sm:ml-4"
     />
   );
 }
