222ec1d75b6687028ce6c6a8954301986bc38475
Charlotte W <152224795+108charlotte@users.noreply.github.com>
2025-08-31 13:57:59 +0000

    updated linkedin link to redirect to actual webpage

--- personal-website/app/Contact.js
+++ personal-website/app/Contact.js
@@ -6,7 +6,7 @@ export default function Contact({ isDarkMode }) {
           <h2>Contact Me</h2>
             <p>Feel free to reach out to me about anything--I&apos;m open to questions, project ideas, or just saying hi! </p>
             <p>Email: <a className="email underline hover:no-underline transition-colors font-medium" href="mailto: charlottewoodrum@westminster.net">charlottewoodrum@westminster.net</a></p>
-            <p className="mt-4">Connect with me on <a className="email underline hover:no-underline transition-colors font-medium" href="www.linkedin.com/in/charlotte-woodrum-557900370">LinkedIn</a>!</p>
+            <p className="mt-4">Connect with me on <a className="email underline hover:no-underline transition-colors font-medium" href="https://www.linkedin.com/in/charlotte-woodrum-557900370">LinkedIn</a>!</p>
         </FadeIn>
     )
 }
